ec846ec511d422a82465d4229cb219b8843ead0f
tendai-wbm <tendai@cleverfranke.com>
2026-05-01 14:36:41 +0200

    meaningful response message to go along with 404

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import http from 'http';
 
 const PORT = 5000
 const server = http.createServer((req,res) => {
-	res.setHeader('Content-Type','text/plain');
+	res.setHeader('Content-Type','text/html');
 	res.statusCode = 404;
-	res.end("<h3>This is NodeJS</h3>");
+	res.end("Page not found.");
 });
 
 server.listen(PORT, () => { console.log(`SERVER RUNNING ON ${PORT}`); });
