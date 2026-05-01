8cb1cd57c0434a82f0b6656b7c9bdd6581149b3d
tendai-wbm <tendai@cleverfranke.com>
2026-05-01 14:31:51 +0200

    response can also be sent using the end method

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const PORT = 5000
 const server = http.createServer((req,res) => {
 	res.setHeader('Content-Type','text/html');
 	res.statusCode = 404;
-	res.write("<h2>This is NodeJS</h2>");
-	res.end();
+	res.end("<h2>This is NodeJS</h2>");
 });
 
 server.listen(PORT, () => { console.log(`SERVER RUNNING ON ${PORT}`); });
