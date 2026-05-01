ff7d4a653e2432364c357e13d60410bcc770a05a
tendai-wbm <tendai@cleverfranke.com>
2026-05-01 15:40:58 +0200

    manually check urls to determine route & serve resopnse

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ import http from 'http';
 
 const PORT = 5000
 const server = http.createServer((req,res) => {
-	res.writeHead(200, { 'Content-Type': 'json' });
-	res.end(JSON.stringify({"appName": "Tora Mari UTD" }));
+	if (req.url === "/") {
+		res.writeHead(200, { 'Content-Type': 'json' });
+		res.end(JSON.stringify({"appName": "Tora Mari UTD" }));
+	}
+
+	else if (req.url === "/about") {
+		res.writeHead(404, { 'Content-Type': 'json' });
+		res.end(JSON.stringify({"motto": "Mari Hairambwe" }));
+	}
+
+	else {
+		res.writeHead(404, { 'Content-Type': 'text/plain' });
+		res.end("Page Not Found");
+	}
 });
 
 server.listen(PORT, () => { console.log(`SERVER RUNNING ON ${PORT}`); });
