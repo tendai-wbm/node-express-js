ecc81a27449ff918b7f2835313014efe37cf0e98
tendai-wbm <tendai@cleverfranke.com>
2026-05-01 14:41:01 +0200

    writeHead() and send response as JSON

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ import http from 'http';
 
 const PORT = 5000
 const server = http.createServer((req,res) => {
-	res.setHeader('Content-Type','text/html');
-	res.statusCode = 404;
-	res.end("Page not found.");
+	res.writeHead(200, { 'Content-Type': 'json' });
+	res.end(JSON.stringify({"appName": "Tora Mari UTD" }));
 });
 
 server.listen(PORT, () => { console.log(`SERVER RUNNING ON ${PORT}`); });
