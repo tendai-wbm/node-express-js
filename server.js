74a66743bc5958124f83cffe43f5b0bdbe4c6a55
tendai-wbm <tendai@cleverfranke.com>
2026-05-01 14:33:54 +0200

    response as plain text using headers

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import http from 'http';
 
 const PORT = 5000
 const server = http.createServer((req,res) => {
-	res.setHeader('Content-Type','text/html');
+	res.setHeader('Content-Type','text/plain');
 	res.statusCode = 404;
-	res.end("<h2>This is NodeJS</h2>");
+	res.end("<h3>This is NodeJS</h3>");
 });
 
 server.listen(PORT, () => { console.log(`SERVER RUNNING ON ${PORT}`); });
